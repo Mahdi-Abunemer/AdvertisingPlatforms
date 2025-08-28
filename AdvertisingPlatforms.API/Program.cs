6c02d4071e7c6384bbb1b1d9ced9454376e48e1a
Mahdi-Abunemer <163647199+Mahdi-Abunemer@users.noreply.github.com>
2025-08-28 17:21:19 +0700

    Create Upload Controller and configure the DI and middleware in startup.

--- AdvertisingPlatforms.API/Program.cs
+++ AdvertisingPlatforms.API/Program.cs
@@ -1,12 +1,22 @@
+using AdvertisingPlatforms.Application.Interfaces;
+using AdvertisingPlatforms.Application.Services;
+using AdvertisingPlatforms.Infrastructure.Repositories;
+using AdvertisingPlatforms.WebApi.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 
 builder.Services.AddControllers();
 
+builder.Services.AddSingleton<IAdvertisingPlatformRepository, InMemoryAdvertisingPlatformRepository>();
+
+builder.Services.AddScoped<FileUploadService>();
+
 var app = builder.Build();
 
 
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.UseHttpsRedirection();
 
